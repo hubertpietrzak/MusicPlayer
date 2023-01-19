a38a3d508c952b56c17cad20e42790614be9f9a4
hubertpietrzak <hubert.pietrzak95@gmail.com>
2023-01-19 13:29:12 +0100

    FXML file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module MusicPlayer {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports org.hubertpietrzak.musicPlayer.main to javafx.graphics;
+}
--- src/main/java/org/hubertpietrzak/Main.java
+++
@@ -1,7 +0,0 @@
-package org.hubertpietrzak;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}

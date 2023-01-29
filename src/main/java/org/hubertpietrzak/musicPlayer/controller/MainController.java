786d7d7c8827007a7804d472264a556e83495f94
hubertpietrzak <hubert.pietrzak95@gmail.com>
2023-01-29 21:12:58 +0100

    communication

--- src/main/java/org/hubertpietrzak/musicPlayer/controller/MainController.java
+++ src/main/java/org/hubertpietrzak/musicPlayer/controller/MainController.java
@@ -1,15 +1,19 @@
 package org.hubertpietrzak.musicPlayer.controller;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseEvent;
 
 public class MainController {
+    @FXML
+    private ContentPaneController contentPaneController;
+    @FXML
+    private ControlPaneController controlPaneController;
+    @FXML
+    private MenuPaneController menuPaneController;
 
     public void initialize() {
         System.out.println("Main controller created");
+        System.out.println(contentPaneController);
+        System.out.println(controlPaneController);
+        System.out.println(menuPaneController);
     }
 }

e581b7332b1d63b47132829572545b544630e79c
hubertpietrzak <hubert.pietrzak95@gmail.com>
2023-01-30 00:17:49 +0100

    CSS styles

--- src/main/java/org/hubertpietrzak/musicPlayer/controller/ControlPaneController.java
+++ src/main/java/org/hubertpietrzak/musicPlayer/controller/ControlPaneController.java
@@ -25,12 +25,16 @@ public class ControlPaneController {
     public void initialize() {
         System.out.println("Control controller created");
         configureButtons();
-        configureVolume();
+        configureSliders();
     }
 
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
-                System.out.println("Wciśnięto przycisk na suwaku głośności")
+    private void configureSliders() {
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                System.out.println("Zmiana głośności " + newValue.doubleValue())
+        );
+
+        progressSlider.valueProperty().addListener(x ->
+                System.out.println("Przesunięcie piosenki")
         );
     }
 
